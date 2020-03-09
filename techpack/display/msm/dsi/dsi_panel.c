b20bca50110c55a5aeff4837960b6f11b3be6cb2
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    techpack: display: msm: Adapt exposure adjustment methods for sm8250
    - This commit was originally made for davinci but it helps for us too as it retains backlight level prior to enabling DC Dim.
    
    [muralivijay: Adapt to spes]
    [fadlyas07: Remove config guards.]
    [johnmart19: Adapt for sm8250]
    
    Co-authored-by: muralivijay <muralidharan9845@gmail.com>
    Co-authored-by: fadlyas07 <mhmmdfdlyas@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -19,6 +19,7 @@
 #include "dsi_display.h"
 #include "sde_dbg.h"
 #include "dsi_mi_feature.h"
+#include "exposure_adjustment.h"
 
 /**
  * topology is currently defined by a set of following 3 values:
@@ -938,12 +939,16 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
+        int bl_dc_min = panel->bl_config.bl_min_level * 2;
 	struct dsi_backlight_config *bl = &panel->bl_config;
 	struct dsi_backlight_config *bl_slaver = &panel->bl_slaver_config;
 	struct dsi_panel_mi_cfg *mi_cfg = &panel->mi_cfg;
 
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
+
+        if (bl_lvl > 0)
+                bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
 
 	/* lmi panel must restore to last_bl_level to avoid flash high
 	 * brightness white exiting app lock with DC on (MIUI-1755728),
